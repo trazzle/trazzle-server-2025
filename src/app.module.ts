a16b39eaba5e273a27da413e914733d729c3d620
eunkang-choi <dmsrkd1216@gmail.com>
2025-11-08 00:30:16 +0900

    ✨ Init: 비즈니스로직 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
 import { RedisCacheModule } from './redis-cache/redis-cache.module';
 import { DiscordsModule } from './discords/discords.module';
+import { PoliciesModule } from './policies/policies.module';
+import { MembersModule } from './members/members.module';
+import { CountriesModule } from './countries/countries.module';
+import { CitiesModule } from './cities/cities.module';
+import { MagnetsModule } from './magnets/magnets.module';
+import { TravelsModule } from './travels/travels.module';
+import { AwsModule } from './aws/aws.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(configModuleOptions), PrismaModule, RedisCacheModule, DiscordsModule],
+  imports: [ConfigModule.forRoot(configModuleOptions), PrismaModule, RedisCacheModule, DiscordsModule, PoliciesModule, MembersModule, CountriesModule, CitiesModule, MagnetsModule, TravelsModule, AwsModule],
   controllers: [AppController],
   providers: [],
 })
