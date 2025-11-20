47999fd890da67e419afd20e653cd52505bafc9c
eunkang-choi <dmsrkd1216@gmail.com>
2025-11-21 03:19:32 +0900

    Feat: 로그인 유저 정보 캐싱 구현..

--- src/users/users.cache-repository.ts
+++ src/users/users.cache-repository.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { RedisCacheService } from '../redis-cache/redis-cache.service';
 import {
   IUsersCacheRepository,
+  SIGN_IN_USERINFO_KEY,
   USER_ACCESS_TOKEN_KEY,
   USER_REFRESH_TOKEN_KEY,
 } from './users.cache-repository.interface';
+import { TrazzleUser } from '../auth/trazzle-user.interface';
 
 @Injectable()
 export class UserCacheRepositoryImpl implements IUsersCacheRepository {
@@ -33,5 +35,27 @@ export class UserCacheRepositoryImpl implements IUsersCacheRepository {
   async delUserRefreshToken(id: number): Promise<void> {
     const key = USER_REFRESH_TOKEN_KEY(id);
     await this.redis.del(key);
+  }
+  async setSignInUserInfo(accessToken: string, value: TrazzleUser): Promise<void> {
+    const key = SIGN_IN_USERINFO_KEY(accessToken);
+
+    // 직렬화
+    const serializedValue = JSON.stringify(value);
+    await this.redis.set(key, serializedValue, 3600); // TTL: 1Hour
+  }
+  async getSignInUserInfo(accessToken: string): Promise<TrazzleUser | null> {
+    const key = SIGN_IN_USERINFO_KEY(accessToken);
+
+    const cachedData = await this.redis.get(key);
+    if (typeof cachedData !== 'string') {
+      return null;
+    }
+
+    try {
+      // 역직렬화
+      return JSON.parse(cachedData) as TrazzleUser;
+    } catch {
+      return null;
+    }
   }
 }
